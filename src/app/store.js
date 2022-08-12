f1d57ff622e1f58c2df39cfe289262be07285b2d
audreyper <audreyper@protonmail.com>
2022-08-12 14:41:05 -0400

    first commit

---
+++ src/app/store.js
@@ -0,0 +1,15 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { favoriteRecipesReducer } from '../features/favoriteRecipes/favoriteRecipesSlice.js';
+import { searchTermReducer } from '../features/searchTerm/searchTermSlice.js';
+import { allRecipesReducer } from '../features/allRecipes/allRecipesSlice.js';
+
+export const store = configureStore({
+    reducer: {
+      favoriteRecipes: favoriteRecipesReducer,
+      searchTerm: searchTermReducer,
+      allRecipes: allRecipesReducer
+    },
+  })
+  
+
+
---
+++ src/util/data.js
@@ -0,0 +1,15 @@
+const allRecipesData = [
+    { id: 0, name: 'Biscuits', img: 'https://static01.nyt.com/images/2020/12/13/dining/ek-lofthouse/merlin_180932583_7cf08465-f282-4d4c-a1c7-4a770fd10a72-articleLarge.jpg'},
+    { id: 1, name: 'Bulgogi', img: 'https://www.justataste.com/wp-content/uploads/2019/03/beef-bulgogi-bowl-recipe.jpg'},
+    { id: 2, name: 'Calamari', img: 'https://www.thespruceeats.com/thmb/xBoL0LGvHA-WGVZSrXNMj0yqmpI=/1332x1332/smart/filters:no_upscale()/fried-calamari-2019531-hero-01-e294f265d7e84c0f89d4b5759759ffbf.jpg'},
+    { id: 3, name: 'Ceviche', img: 'https://www.cookingclassy.com/wp-content/uploads/2020/09/ceviche-6.jpg'},
+    { id: 4, name: 'Cheeseburger', img: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/copycat-western-bacon-cheeseburger-2-1649170836.jpg'},
+    { id: 5, name: 'Churrasco', img: 'https://i0.wp.com/www.sweetteaandthyme.com/wp-content/uploads/2016/05/Churrasco-hero-shot.jpg?fit=1000%2C1500&ssl=1'},
+    { id: 6, name: 'Dumplings', img: 'https://mamalovestocook.com/wp-content/uploads/2020/05/boiled-dumplings-480x480.jpg'},
+    { id: 7, name: 'Fish & Chips', img: 'https://simply-delicious-food.com/wp-content/uploads/2020/02/Fish-and-chips-4.jpg'},
+    { id: 8, name: 'Hummus', img: 'https://cdn.loveandlemons.com/wp-content/uploads/2019/12/hummus-500x375.jpg'},
+    { id: 9, name: 'Masala Dosa', img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxNh6m8bc82v1aCAGxPcj9rtfghBc1zGu--Q&usqp=CAU'},
+    { id: 10, name: 'Pad Thai', img: 'https://www.chilipeppermadness.com/wp-content/uploads/2022/03/Pad-Thai-SQ-500x375.jpg'},
+  ];
+  
+  export default allRecipesData;
